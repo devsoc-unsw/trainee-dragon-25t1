5e3ff0428859dd882b6fe8bbcb195c0f2dc57dbe
Yanlin Li <yanauslin8@gmail.com>
2025-05-21 21:31:25 +1000

    added a visual list of the restaurants on the map

---
+++ frontend/spotz/src/Components/PlaceDetails/types.ts
@@ -0,0 +1,46 @@
+export interface Place {
+  address: string;
+  awards: Award[];
+  cuisine: Cuisine[];
+  name: string;
+  num_reviews: number;
+  phone: string;
+  photo: Photo | any;
+  price_level: number | string;
+  ranking: string;
+  rating: number;
+  website: any;
+  web_url: any;
+}
+
+export interface Cuisine {
+  key: number;
+  name: string;
+}
+
+export interface Award {
+  display_name: string;
+  images: any;
+}
+
+export interface Photo {
+  caption: string;
+  helpful_votes: number;
+  images: Images;
+  is_blessed: true;
+  published_date: any; // Date obj?
+  uploaded_date: any; // Date obj?
+}
+
+interface Images {
+  large: ImageContent;
+  medium: ImageContent;
+  small: ImageContent;
+  thumbnail: ImageContent;
+}
+
+interface ImageContent {
+  width: number;
+  height: number;
+  url: any;
+}
