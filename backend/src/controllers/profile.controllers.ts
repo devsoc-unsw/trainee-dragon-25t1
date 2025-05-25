e49a86e661350eac4583e11bdbf73922e75b5621
Mark Fernandez <143058147+markf001@users.noreply.github.com>
2025-05-26 03:18:14 +1000

    updating branch

--- backend/src/controllers/profile.controllers.ts
+++ backend/src/controllers/profile.controllers.ts
@@ -67,44 +67,22 @@ async function getDislikes(req: Request, res: Response) {
   }
 }
 
-<<<<<<< HEAD
-async function getLikes(req: Request, res: Response) {
-  try {
-    const session = req.header('session')!;
-    const likes = profileService.fetchLikes(session);
-    res.json({ likes });
-  }
-  
-  catch (err: any) {
-=======
 async function clearHistory(req: Request, res: Response) {
   try {
     const session = req.cookies.sessionId;
     const profile = profileService.clearHistory(session as string);
     res.json(profile);
   } catch (err: any) {
->>>>>>> 5fa8f3f167d3ddd9d9a28a28c69bc65c003cfb2d
     res.status(400).json({ error: err.message });
   }
 }
 
-<<<<<<< HEAD
-async function getDislikes(req: Request, res: Response) {
-  try {
-    const session = req.header('session')!;
-    const dislikes = profileService.fetchDislikes(session);
-    res.json({ dislikes });
-  }
-  
-  catch (err: any) {
-=======
 async function clearBookmarks(req: Request, res: Response) {
   try {
     const session = req.cookies.sessionId;
     const profile = profileService.clearBookmarks(session as string);
     res.json(profile);
   } catch (err: any) {
->>>>>>> 5fa8f3f167d3ddd9d9a28a28c69bc65c003cfb2d
     res.status(400).json({ error: err.message });
   }
 }
